3ddb70c05c3330e6e0cacb8f09e3a5b21424725b
Saul Medina <saul77023@gmail.com>
2022-07-04 19:07:48 -0500

    Se implemento una mejora en la el apartado de la consola ahora el usuario tiene mas facilidad de uso

--- consola.java
+++ consola.java
@@ -10,30 +10,97 @@ public class consola {
         FormulasIMP formula = new FormulasIMP();
         Scanner inputconsole = new Scanner(System.in);
 
+        int opc = 0;
         double numero1 = 0 , numero2 = 0 , resultado = 0;
 
         try {
 
-            System.out.println("Dame un numero: ");
-            numero1 = Double.parseDouble(inputconsole.nextLine());
+            System.out.println("Bienbenido que tipo de problema quieres resolver? Digita el numero."
+            + "\n 1- Matematicas "
+            + "\n 2- Fisica "
+            + "\n 3- Salir");
 
-            System.out.println("Dame un numero mas: ");
-            numero2 = Double.parseDouble(inputconsole.nextLine());
+            opc = Integer.parseInt(inputconsole.nextLine());
 
-            resultado = formula.sumar(numero1,numero2);
-            System.out.println("resultado suma = " + resultado);
+            do {
+                if (opc != 3) {
+                    switch (opc) {
+                        case 1:
 
-            resultado = formula.dividir(numero1 , numero2);
-            System.out.println("resultado divicion = " + resultado);
+                            System.out.println("---Tipo de operaciones---"
+                                    + "\n 1-Sumar"
+                                    + "\n 2-Restar"
+                                    + "\n 3-Multiplicar"
+                                    + "\n 4-Dividir"
+                                    + "\n 5-Promediar"
+                                    + "\n 6-Regresar");
 
-            resultado = formula.restar(numero1 , numero2);
-            System.out.println("resultado resta = " + resultado);
+                            opc = Integer.parseInt(inputconsole.nextLine());
 
-            resultado = formula.promediar(numero1 , numero2);
-            System.out.println("resultado promedio = " + resultado);
+                            switch (opc) {
+                                case 1:
+                                    System.out.println("Dame un numero: ");
+                                    numero1 = Double.parseDouble(inputconsole.nextLine());
 
-            resultado = formula.multiplicar(numero1 , numero2);
-            System.out.println("resultado multiplicar = " + resultado);
+                                    System.out.println("Dame un numero mas: ");
+                                    numero2 = Double.parseDouble(inputconsole.nextLine());
+
+                                    resultado = formula.sumar(numero1, numero2);
+                                    System.out.println("resultado suma = " + resultado);
+                                    break;
+
+                                case 2:
+                                    System.out.println("Dame un numero: ");
+                                    numero1 = Double.parseDouble(inputconsole.nextLine());
+
+                                    System.out.println("Dame un numero mas: ");
+                                    numero2 = Double.parseDouble(inputconsole.nextLine());
+
+                                    resultado = formula.restar(numero1, numero2);
+                                    System.out.println("resultado resta = " + resultado);
+                                    break;
+
+                                case 3:
+                                    System.out.println("Dame un numero: ");
+                                    numero1 = Double.parseDouble(inputconsole.nextLine());
+
+                                    System.out.println("Dame un numero mas: ");
+                                    numero2 = Double.parseDouble(inputconsole.nextLine());
+
+                                    resultado = formula.multiplicar(numero1, numero2);
+                                    System.out.println("resultado multiplicar = " + resultado);
+                                    break;
+                                case 4:
+
+
+                                    resultado = formula.dividir(numero1, numero2);
+                                    System.out.println("resultado divicion = " + resultado);
+                                    break;
+
+                                case 5:
+                                    System.out.println("Dame un numero: ");
+                                    numero1 = Double.parseDouble(inputconsole.nextLine());
+
+                                    System.out.println("Dame un numero mas: ");
+                                    numero2 = Double.parseDouble(inputconsole.nextLine());
+
+                                    resultado = formula.promediar(numero1, numero2);
+                                    System.out.println("resultado promedio = " + resultado);
+                                    break;
+                                case 6:
+                                    break;
+                                default:
+                                    System.out.println("Ocurrio un error o esa opcion no existe.");
+                            }
+                            break;
+
+                        case 2:
+                            break;
+                        default:
+                            System.out.println("Ocurrio un error o esa opcion no existe.");
+                    }
+                }
+            }while (opc != 3);
 
         }catch (InputMismatchException ex){
             System.out.println("Ha ocurrido un problema a el ingreasar datos.");
